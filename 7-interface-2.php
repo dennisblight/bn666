9a33054304d6f43ed60dc3ffb22db384f1bd2180
Dennis Arfan <dennisarfan@gmail.com>
2022-10-18 11:12:07 +0700

    update

---
+++ 7-interface-2.php
@@ -0,0 +1,71 @@
+<?php
+
+// Interface hanya boleh memiliki deklarasi fungsi
+// tanpa method body, dan konstanta yang tidak bisa
+// diubah lagi nilainya
+interface GeometriInterface
+{
+    const Dimensi2 = 2;
+    const Dimensi3 = 3;
+
+    public function getDimensi();
+}
+
+// Interface yang mendefinisikan bangun datar
+// seperti persegi, lingkaran, persegi panjang, segitiga dll
+interface BangunDatarInterface extends GeometriInterface
+{
+    public function getLuas();
+    public function getKeliling();
+}
+
+// Interface yang mendefinisikan bangun ruang
+// seperti kubus, balok, kerucut, silinder dll
+interface BangunRuangInterface extends GeometriInterface
+{
+    public function getVolume();
+    public function getLuasPermukaan();
+}
+
+// Interface yang mendefinisikan sebuah kelas bisa menampilkan
+// informasi
+interface InfoShower
+{
+    public function showInfo();
+}
+
+/************************************************
+ * Implementasi interface terhadap abstract class
+ ************************************************/
+
+abstract class Geometri implements GeometriInterface, InfoShower
+{
+    public function showInfo()
+    {
+        echo "Dimensi: " . $this->getDimensi() . "<br/>";
+    }
+}
+
+abstract class BangunDatar extends Geometri implements BangunDatarInterface
+{
+    public function getDimensi() { return static::Dimensi2; }
+
+    public function showInfo()
+    {
+        parent::showInfo();
+        echo "Luas: " . $this->getLuas() . "<br/>";
+        echo "Keliling: " . $this->getKeliling() . "<br/>";
+    }
+}
+
+abstract class BangunDatar extends Geometri implements BangunDatarInterface
+{
+    public function getDimensi() { return static::Dimensi3; }
+
+    public function showInfo()
+    {
+        parent::showInfo();
+        echo "Volume: " . $this->getVolume() . "<br/>";
+        echo "Luas Permukaan: " . $this->getLuasPermukaan() . "<br/>";
+    }
+}
